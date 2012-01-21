0b80397a145367055e3330be68711b9848d6ef4f
padatko <Paddy@Paddy-PC.(none)>
2012-01-21 20:21:15 +0100

    refactored betting calculation

--- src/main/scala/de/htwg_konstanz/betcalculator/BetCalculator.scala
+++ src/main/scala/de/htwg_konstanz/betcalculator/BetCalculator.scala
@@ -2,32 +2,53 @@ package de.htwg_konstanz.betcalculator
 
 class BetCalculator(
   val allBets: Map[Int, Bet],
-  val systemCombination: Int) {
+  val systemCombination: Int,
+  val wager: Double) {
 
-  def createCombinations(quotes: Set[Double], systemCombination: Int): Set[Set[Double]] = {
-    require(quotes.size > 2)
-    require(systemCombination > 1 && systemCombination < quotes.size)
-    quotes.toList.combinations(systemCombination).toSet map { (list: List[Double]) => list.toSet }
-  }
-
-  def createCombinations2: Set[Set[Bet]] = {
+  def createCombinations: Set[Set[Bet]] = {
     require(allBets.size > 2)
     require(systemCombination > 1 && systemCombination < allBets.size)
-    allBets.values.toList.combinations(systemCombination).toSet map { (t: List[Bet] => t.toSet)}
-//    allBets.combinations(systemCombination).toSet map { (list: List[Bet]) => list.toSet }
-//    allBets.values.toList.combinations(systemCombination).toSet map { (list: List[Double]) => list.toSet }
+    allBets.values.toList.combinations(systemCombination).toSet map { (list: List[Bet]) => list.toSet }
   }
 
-  def calculateOverallQuote(combinations: Set[Set[Double]]): Set[RowQuotes] = combinations map {
-    combination => RowQuotes(combination, combination.product)
+  def calculateOverallQuote(combinations: Set[Set[Bet]]): Set[RowQuotes] = combinations map {
+    combination =>
+      RowQuotes(combination, limitDecimals(generateQuotesList(combination).product, 2))
   }
 
-  def calculateOverallWinnings(combinations: Set[Set[Double]], betPart: Double): Set[RowWinnings] = {
-    calculateOverallQuote(combinations) map {
-      case RowQuotes(combination, quote) => RowWinnings(combination, quote * betPart)
+  private def generateQuotesList(betRow: Set[Bet]) = {
+    betRow.toList.flatMap { e =>
+      try {
+        List(e.quote)
+      } catch {
+        case _ => Nil
+      }
     }
   }
+
+  def calculateOverallWinnings(combinations: Set[Set[Bet]]): Set[RowWinnings] = {
+    calculateOverallQuote(combinations) map {
+      combination =>
+        estimateRowValue(combination.combination) match {
+          case true => RowWinnings(combination.combination, 0.00)
+          case false => RowWinnings(combination.combination, limitDecimals(combination.quote * (wager / combinations.size), 2))
+        }
+    }
+  }
+
+  private def estimateRowValue(row: Set[Bet]): Boolean = {
+    val values = row.toList.flatMap { e =>
+      try {
+        List(e.winning)
+      } catch {
+        case _ => Nil
+      }
+    }
+    values.contains(false)
+  }
+  private def limitDecimals(number: Double, length: Int) = BigDecimal(number).setScale(length, BigDecimal.RoundingMode.FLOOR).toDouble
+
 }
 
-final case class RowQuotes(val combination: Set[Double], quote: Double)
-final case class RowWinnings(val combination: Set[Double], winning: Double)
+final case class RowQuotes(val combination: Set[Bet], quote: Double)
+final case class RowWinnings(val combination: Set[Bet], winning: Double)
--- src/test/scala/de/htwg_konstanz/betcalculator/BetCalculatorTests.scala
+++ src/test/scala/de/htwg_konstanz/betcalculator/BetCalculatorTests.scala
@@ -2,66 +2,91 @@ package de.htwg_konstanz.betcalculator
 
 class BetCalculatorTests extends UnitTestConfiguration {
 
-  private val threeChosenQuotes = Set(1.0, 2.0, 3.0)
-  private var betCalculator: BetCalculator = _
-  private val allBets: Map[Int, Bet] = Map(
-      1 -> Bet(1,1.1), 
-      2 -> Bet(4,1.1), 
-      3 -> Bet(0, 1.3))
-
-  override def beforeEach() {
-    betCalculator = new BetCalculator(allBets, 2)
-  }
+  private val betA = Bet(1, 1.1, true)
+  private val betB = Bet(4, 1.1)
+  private val betC = Bet(0, 1.3, true)
+  private val betD = Bet(2, 2.1)
+  private val threeBets: Map[Int, Bet] = Map(
+    1 -> betA,
+    2 -> betB,
+    3 -> betC)
+  private val fourBets: Map[Int, Bet] = Map(
+    1 -> betA,
+    2 -> betB,
+    3 -> betC,
+    4 -> betD)
+  private val twoOfThreeCombinations: Set[Set[Bet]] = Set(
+    Set(betA, betB),
+    Set(betA, betC),
+    Set(betB, betC))
+  private val twoOfFourCombinations: Set[Set[Bet]] = Set(
+    Set(betA, betB),
+    Set(betC, betD),
+    Set(betA, betC),
+    Set(betB, betD),
+    Set(betA, betD),
+    Set(betB, betC))
+  private val threeOfFourCombinations: Set[Set[Bet]] = Set(
+    Set(betA, betB, betC),
+    Set(betA, betB, betD),
+    Set(betA, betC, betD),
+    Set(betB, betC, betD))
+  private val basicWager = 10.00
 
   test("3 quotes should result in 3 pairs, when invoked with 2 combinations") {
-    val actualPairs = betCalculator.createCombinations2
-    val expectedPairs = Set(Set(1.0, 2.0), Set(2.0, 3.0), Set(3.0, 1.0))
+    val betCalculator = new BetCalculator(threeBets, 2, basicWager)
+    val actualPairs = betCalculator.createCombinations
+    val expectedPairs = twoOfThreeCombinations
 
-    //actualPairs should be(expectedPairs)
+    actualPairs should be(expectedPairs)
   }
 
   test("4 quotes should result in 3 tuple3s, when invoked with 3 combinations") {
-    val chosenQuotes = Set(1.0, 2.0, 3.0, 4.0)
-    val actualPairs = betCalculator.createCombinations(chosenQuotes, 3)
-    val expectedPairs = Set(Set(1.0, 2.0, 3.0), Set(1.0, 2.0, 4.0), Set(2.0, 3.0, 4.0), Set(1.0, 3.0, 4.0))
+    val betCalculator = new BetCalculator(fourBets, 3, basicWager)
+    val actualPairs = betCalculator.createCombinations
+    val expectedPairs = threeOfFourCombinations
 
     actualPairs should be(expectedPairs)
   }
 
   test("4 quotes should result in 6 pairs, when invoked with 2 combinations") {
-    val chosenQuotes = Set(1.0, 2.0, 3.0, 4.0)
-    val actualPairs = betCalculator.createCombinations(chosenQuotes, 2)
-    val expectedPairs = Set(Set(1.0, 2.0), Set(1.0, 3.0), Set(1.0, 4.0), Set(2.0, 3.0), Set(3.0, 4.0), Set(2.0, 4.0))
+    val betCalculator = new BetCalculator(fourBets, 2, basicWager)
+    val actualPairs = betCalculator.createCombinations
+    val expectedPairs = twoOfFourCombinations
 
     actualPairs should be(expectedPairs)
   }
 
   test("BetRowCreator should throw an exception when createCombinations is invoked with less than 3 quotes") {
-    val chosenQuotes = Set(1.0, 2.0)
-    evaluating { betCalculator.createCombinations(chosenQuotes, 2) } should produce[IllegalArgumentException]
+    val betCalculator = new BetCalculator(Map(1 -> betA, 2 -> betB), 2, basicWager)
+    evaluating { betCalculator.createCombinations } should produce[IllegalArgumentException]
   }
 
   test("BetRowCreator should throw an exception when less than 2 combinations are invoked on 3 quotes") {
-    evaluating { betCalculator.createCombinations(threeChosenQuotes, 1) } should produce[IllegalArgumentException]
+    val betCalculator = new BetCalculator(Map(1 -> betA, 2 -> betB), 1, basicWager)
+    evaluating { betCalculator.createCombinations } should produce[IllegalArgumentException]
   }
 
   test("BetRowCreator should throw an exception when more than 2 combinations are invoked on 3 quotes") {
-    evaluating { betCalculator.createCombinations(threeChosenQuotes, 3) } should produce[IllegalArgumentException]
+    val betCalculator = new BetCalculator(Map(1 -> betA, 2 -> betB), 2, basicWager)
+    evaluating { betCalculator.createCombinations } should produce[IllegalArgumentException]
   }
 
   test("Given 3 pairs calculateOverallQuote should return overall results") {
-    val pairs = Set(Set(1.0, 2.0), Set(2.0, 3.0), Set(3.0, 1.0))
-    val actualSetOfOverallQuotes = betCalculator.calculateOverallQuote(pairs)
-    val expectedSetOfOverallQuotes = Set(RowQuotes(Set(1.0, 2.0), 2.0), RowQuotes(Set(2.0, 3.0), 6.0), RowQuotes(Set(3.0, 1.0), 3.0))
+    val betCalculator = new BetCalculator(threeBets, 2, basicWager)
+    val combinations = betCalculator.createCombinations
+    val actualSetOfOverallQuotes = betCalculator.calculateOverallQuote(combinations)
+    val expectedSetOfOverallQuotes = Set(RowQuotes(Set(betA, betB), 1.21), RowQuotes(Set(betA, betC), 1.43), RowQuotes(Set(betB, betC), 1.43))
 
     actualSetOfOverallQuotes should be(expectedSetOfOverallQuotes)
   }
 
   test("Given 3 pairs calculateOverallWinning should return overall winning") {
-    val pairs = Set(Set(1.0, 2.0), Set(2.0, 3.0), Set(3.0, 1.0))
-    val actualSetOfOverallWinnings = betCalculator.calculateOverallWinnings(pairs, 2.0)
-    val expectedSetOfOverallWinnings = Set(RowWinnings(Set(1.0, 2.0), 4.0), RowWinnings(Set(2.0, 3.0), 12.0), RowWinnings(Set(3.0, 1.0), 6.0))
-
+    val betCalculator = new BetCalculator(threeBets, 2, 10.00)
+    val combinations = betCalculator.createCombinations
+    val actualSetOfOverallWinnings = betCalculator.calculateOverallWinnings(combinations)
+    val expectedSetOfOverallWinnings = Set(RowWinnings(Set(betA, betB), 0.00), RowWinnings(Set(betB, betC), 0.00) , RowWinnings(Set(betA, betC), 4.76))
+    
     actualSetOfOverallWinnings should be(expectedSetOfOverallWinnings)
   }
 
